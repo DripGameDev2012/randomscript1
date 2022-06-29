746055989cfaed85dcb4ddc5db35e5321c2bf90d
Plun <102840087+DripGameDev2012@users.noreply.github.com>
2022-06-29 14:23:21 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+	class Mailbox
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Tip: Try saying \"Do I have mail?\" ");
+			bool mail;
+			var what = Console.ReadLine();
+			if (what == "Do I have mail?")
+			{
+				Random random = new Random();
+				int integer = random.Next(0, 2);
+				Console.WriteLine(integer);
+				if (integer == 1)
+				{
+					mail = true;
+					if (mail == true)
+					{
+						Mailbox1("is");
+						Console.WriteLine("I'd recommend to now say \"How much mail is there?\" ");
+						var what1 = Console.ReadLine();
+						if
+
+					}
+				}
+				else
+				{
+					mail = false;
+					if (mail == false)
+					{
+						Mailbox1("is no");
+
+					}
+				}
+			}
+            else
+            {
+				Console.WriteLine("Sorry, but that wasn't typed correctly.");
+            }
+		}
+		public static void Mailbox1(string ismail)
+		{
+			Console.WriteLine("There {0} mail", ismail);
+
+		}
+		public void Mailbox2(int howmuch)
+        {
+			Console.WriteLine("There is {0} pieces of mail", howmuch);
+        }
+	}
+}
